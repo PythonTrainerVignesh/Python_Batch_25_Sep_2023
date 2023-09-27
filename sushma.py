f2a4c9153ae6d23a5e5506a5777383bb7c32ad28
sushma@-1234 <sushmareddymaruri@gmail.com>
2023-09-27 18:35:43 +0530

    changed

--- sushma.py
+++ sushma.py
@@ -9,3 +9,9 @@ print("dhaivik ","raja")
 print(3*4*5)
 print(321*5)
 print(10<6,6>2)
+
+cli=command line interference
+gui=graphical user interference
+
+in python there are3 types libraries
+inbuild,create,3rd phase libraries
