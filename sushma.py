96a32c8577a6400beff71990410bb1f5ce2bae42
sushma@-1234 <sushmareddymaruri@gmail.com>
2023-09-28 15:50:55 +0530

    task done

--- sushma.py
+++ sushma.py
@@ -16,3 +16,9 @@ gui=graphical user interference
 in python there are3 types libraries
 #inbuilt libraries
 #3rd phase libraries
+
+TODAY:28/9/2023
+#print("i'm learning python")
+#print(321+456+754)
+#print(990*4)
+#print(465<798,956>456)
