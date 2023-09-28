cfe7ac4111e911314235667dd6c39432c14d4c7b
sushma@-1234 <sushmareddymaruri@gmail.com>
2023-09-28 16:01:38 +0530

    done

--- sushma.py
+++ sushma.py
@@ -13,10 +13,10 @@ print(10<6,6>2)
 cli=command line interference
 gui=graphical user interference
 
-in python there are3 types libraries
+#in python there are3 types libraries
 #inbuilt libraries
 #3rd phase libraries
-
+#create libraries
 TODAY:28/9/2023
 #print("i'm learning python")
 #print(321+456+754)
