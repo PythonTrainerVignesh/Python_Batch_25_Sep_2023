aa4c05d738685bfd2efe515f80a91184d6a251b6
sureshcrizayn <iloveu1000timez@gmail.com>
2023-09-29 02:34:57 -0700

    done

---
+++ suresh1.py
@@ -0,0 +1,10 @@
+print("hellow world")
+
+
+print("hi","my","friend",sep=" ")
+
+print("Programming","Essentials","in",sep="***",end="...")
+print("Python")
+
+
+print("\"I\"","\"\"wanna\"\"","\"\"\"\"become\"\"\"\"","\"\"\"\"rich\"\"\"\"",sep="\n")
