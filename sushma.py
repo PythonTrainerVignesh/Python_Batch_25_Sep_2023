b958e3d92289d22b27d431ee79496daa4f36c2d9
sushma@-1234 <sushmareddymaruri@gmail.com>
2023-09-29 14:44:24 +0530

    .\sushma.py

--- sushma.py
+++ sushma.py
@@ -18,12 +18,12 @@ print(10<6,6>2)
 #3rd phase libraries
 #create libraries
 TODAY:28/9/2023
-#print("i'm learning python")
-#print(321+456+754)
-#print(990*4)
-#print(465<798,956>456)
+print("i'm learning python")
+print(321+456+754)
+print(990*4)
+print(465<798,956>456)
 
-<<<<<<< HEAD
+
 #in print("hello world")print is function,it must have open and close paranthesis("")
 #positional argument we can use(,)
 #key word arguments are two types
@@ -33,10 +33,8 @@ TODAY:28/9/2023
 #\t means tab space it will give much gap
 #\escape character this is wont visible
 #(\n)add new line
-=======
-#positional argument,python we cane use,
-#keyword argument,separate,end
-#print is function
-\n means new line
-we can add both sep,\n  directly.
->>>>>>> f02d6e04b4b66b3d7f406ee74dc8880082a313de
+print("programming","essentials","in", sep='***', end='')
+print("...python") 
+print("\"I'm\" \n\"\"learning\"\" \n\"\"\"python\"\"\"")
+print("one","two","three", sep="\n")
+print("\"I\"","\"\"\"have\"\"\"","\"\"completed\"\"","\"\"\"degree\"\"\"", sep="\n")
