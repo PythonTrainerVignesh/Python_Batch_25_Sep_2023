ffbad9a6e407ad76fbaee7d6747f7a519a4bd4f6
sushmareddy-1234 <145994626+sushmareddy-1234@users.noreply.github.com>
2023-10-01 14:54:25 +0530

    Update sushma.py 1/10/23

--- sushma.py
+++ sushma.py
@@ -30,6 +30,44 @@ print(465<798,956>456)
 #sep("one","two","three")
 #end(***)
 #we can use sep\n at a time,and also end.
+
+TODAY:1/10/23
+#in python data types it has numeric=integer,float,complex numbers
+#it has dictionary
+#and has boolean(true ,false)
+#set
+#sequence number=tuple,string,list
+
+#python data types examples
+#data type output:str
+print("Hello World")
+
+#data type output:int
+x = 60
+
+#data type output:float(decimal value)
+x = 60.5
+
+#data type output:complex
+x = 2+4j
+
+#data type output:list
+x = ["raj", "dhaivik", "sushma"]
+
+#data type output:tuple
+x = ("mom", "dad", "me")
+
+#data type output:dict
+x = {"name": "sushma", "age": 21}
+
+#data type output:set
+x = {"trees", "fruits", "flowers"}
+
+#data type output:boolean
+x = 'True'
+
+
+
 #\t means tab space it will give much gap
 #\escape character this is wont visible
 #(\n)add new line
