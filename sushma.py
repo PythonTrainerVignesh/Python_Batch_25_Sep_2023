7f8ef7e5a0a821f2a840bc5257d40d63afac4a15
sushma@-1234 <sushmareddymaruri@gmail.com>
2023-10-02 18:37:53 +0530

    .\sushma.py

--- sushma.py
+++ sushma.py
@@ -11,7 +11,7 @@ print(321*5)
 print(10<6,6>2)
 
 #cli=command line interference
-#gui=graphical user interference
+#gui=graphical user interference               
 
 #in python there are3 types libraries
 #inbuilt libraries
@@ -32,15 +32,15 @@ print(465<798,956>456)
 #we can use sep\n at a time,and also end.
 #\t means tab space it will give much gap
 #\escape character this is wont visible
-#(\n)add new line
-<<<<<<< HEAD
+#(\n)add new
+
 print("programming","essentials","in", sep='***', end='')
 print("...python") 
 print("\"I'm\" \n\"\"learning\"\" \n\"\"\"python\"\"\"")
 print("one","two","three", sep="\n")
 print("\"I\"","\"\"\"have\"\"\"","\"\"completed\"\"","\"\"\"degree\"\"\"", sep="\n")
-=======
-=======
+
+
 #positional argument,python we cane use,
 #keyword argument,separate,end
 #print is function
@@ -48,4 +48,66 @@ print("\"I\"","\"\"\"have\"\"\"","\"\"completed\"\"","\"\"\"degree\"\"\"", sep="
 #we can add both sep,\n  directly.
 #we can use end ,sep at a time.
 #to open python coding in cmd we want to type python<filename>
->>>>>>> f5bf73a725c0120335898f6f6de7a0c40db76932
+
+
+TODAY:1/10/23
+#in python data types it has numeric=integer,float,complex numbers
+#it has dictionary
+#and has boolean(true ,false)
+#set
+#sequence number=tuple,string,list
+
+#python data types examples
+#data type output:str
+print("Hello World")
+
+#data type output:int
+x = 60
+
+#data type output:float(decimal value)
+x = 60.5
+
+#data type output:complex
+x = 2+4j
+
+#data type output:list
+x = ["raj", "dhaivik", "sushma"]
+
+#data type output:tuple
+x = ("mom", "dad", "me")
+
+#data type output:dict
+x = {"name": "sushma", "age": 21}
+
+#data type output:set
+x = {"trees", "fruits", "flowers"}
+
+#data type output:boolean
+x = 'True'
+
+TODAY:2/10/23
+#python list using []
+var = ["table", "books", "pen"]
+print(var)
+
+#python string example
+print("A Computer science portal for geeks")
+print('A')
+
+#sets can be created by using built-in function()
+set1 = set("GeeksForGeeks")
+print("\nSet with the use of an object: " )
+print(set1)
+
+#in,python tuples are created by placing sequence of values separated by comma
+#creating an empty tuple
+Tuple1 = ()
+print("Intial empty Tuple: ")
+print(Tuple1)
+
+#python dictionary example
+Dict = {1: 'Geeks' , 2: 'For' , 3: 'Geeks'}
+print(Dict)
+
+
+
