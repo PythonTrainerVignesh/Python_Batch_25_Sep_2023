2b53beb71c5a875a35917096c13722a311357afb
unknown <ssimon68475@gmail.com>
2023-10-02 19:16:05 +0530

    added

--- simon28-09.py
+++ simon28-09.py
@@ -13,3 +13,5 @@ print("no","one","change",sep="\n")
 print("\"i'm\"","\"\"a\"\"","\"\"\"fan\"\"\"","\"\"\"\"of\"\"\"\"","\"\"\"\"\"ronaldo\"\"\"\"\"" ,sep="\n")
 
 print(3+3,"b","c",sep=" ")
+print("Ronaldo Great Of All Time \n Goat Cr7",end=" ")
+print ("07")
