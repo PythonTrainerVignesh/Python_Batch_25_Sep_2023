7aa554307a82637f117ec1e2a84b820ac7511d3d
PythonTrainerVignesh <vigneshkumar9797@gmail.com>
2023-10-03 17:56:17 +0530

    new

---
+++ Vignesh_3_10_2023.py
@@ -0,0 +1,29 @@
+# Source Code
+# Print is a function
+print(5)
+
+# Python modules
+# 1. During installation
+# 2. Own repo
+# 3. 3rd Party like GitHub.com
+
+# Python Data Types
+# 1. String (Ch)
+print('Hello World')
+print("Hello World")
+print('Hello "" World')
+print("Hello '' World")
+print("Python's Class")
+print("Python\nClass")
+print("Python\tClass")
+
+# Positional Arguments
+print(5, 6, 8, 'Hello', 55.5, (5 + 8), True, False, (5 - 5))
+# Escape Character (\)
+# Research why \n and \t is not escaping
+print('I\'m', 'learning', 'Python')
+
+# Keyword Arguments
+print('Hello', 'World', end='')
+print('Hello', 'World', sep='')
+print('Hello', 'World', sep='', end='')
