518cb06c1355b37f5c01b1991c5b82a9817bbcf7
unknown <ssimon68475@gmail.com>
2023-10-03 17:40:51 +0530

    some

---
+++ simon-3-10-23.py
@@ -0,0 +1,22 @@
+# source code
+# print is a function
+# IDE Integrated development environment
+# python with integrated folder in pycharm
+# PEP python enchancement proposal
+# PEP is using for style code
+# python modules
+# 1.during installation
+# 2.own repo
+# 3.3rd party like github.com
+# python data types
+# 1.sting ()ch
+# 1.1 print('')
+# 1.2 print("")
+
+# positional argument
+print ('hello','simon')
+print(10+10)
+print("python\tclass")
+
+
+
