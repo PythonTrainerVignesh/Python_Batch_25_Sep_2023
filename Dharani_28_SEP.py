4fa1137150524da76a9bdddd35a715bc0365085b
Dharani-12 <p.dharani225@gmail.com>
2023-10-03 14:25:03 +0530

    add new file Dharani_28_SEP.py

---
+++ Dharani_28_SEP.py
@@ -0,0 +1,50 @@
+print("Programming", "Essential", "in", sep= '***', end='')
+print("...Python\n\n")
+
+print("\"I'm\" \n \"\"Learning\"\"\n \"\"\"Python\"\"\"", end="\n\n")
+
+a=1
+b=4
+c=8
+
+print(a, b, c,  sep=", ", end=".....")
+print("This is on the same line as the previous print statement.\n\n")
+
+
+# To form a list of items:
+
+fruits = ["Apple", "Mango", "cherry", "Papaya"]
+print("FRUITS:", fruits, sep=", ", end="\n\n")
+
+# To make another example:
+
+name= "jackie"
+age= "48"
+city= "England"
+
+print("Name:", name, "Age:", age, "City:", city, sep=" | ", end="\n\n")
+
+
+# Another one
+
+temperature = 30.6
+
+humidity = 60
+
+print(f"Temperature: {temperature} celsius", f"Humidity: {humidity}%", sep="\n", end="\n\n\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
