7f592e9e0cbcbe3dbd8ed6e9535c00b753171e55
unknown <ssimon68475@gmail.com>
2023-10-04 17:22:23 +0530

    done

--- simon-3-10-23.py
+++ simon-3-10-23.py
@@ -8,7 +8,7 @@
 # 1.during installation
 # 2.own repo
 # 3.3rd party like github.com
-# python data types
+# python data types or python literals
 # 1.sting ()ch
 # 1.1 print('')
 # 1.2 print("""")
@@ -20,11 +20,42 @@ print ('hello','simon')
 print("hello","world",sep=" ")
 print("hello","world",end=" ")
 print("hello","world",sep=" ",end=" ")
+print("ending")
 
+# 2. integer
+# print (45)
 
-
+print(100*4) # wholle number
 print(10+10)
 print("python\tclass")
+# 3. floatin point integer
+print(10.23)
 
 
+# 4. boolean is a camel case
+# print(True)
+# print(False)
+# print(4>5)
 
+# python operators(7) or comparison operator(6)
+
+#print(8+8)addition
+#print(8-8)subraction
+#print(8*8)multiplication
+#print(8%8)modules
+#print(8/8)division(the answer will always be in float)
+#print(8**8)power/exponetial
+#print(8//8) floor division
+
+#comparison operator
+#print(<) lesser than
+#print(>)greater than
+#print(<=)lesser than or equal to
+#print(>=)greater than or equal to
+#print(!=)not equal to
+#print(==)similer
+
+# bodmas rule
+print(2+3-2*4/4-(33/5*4))
+print(2%2%2)
+print(2*4%5)
