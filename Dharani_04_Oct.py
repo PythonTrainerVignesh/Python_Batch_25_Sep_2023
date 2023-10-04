5489c447c806eedce739fa945efd0d13f3d389a4
dharani-12 <p.dharani225@gmail.com>
2023-10-04 18:03:43 +0530

    New file

---
+++ Dharani_04_Oct.py
@@ -0,0 +1,56 @@
+# To class notes
+# Data Types or Python Literals
+# 2.Integers:
+print(50)
+
+# Integers/Float?Boolean inside "" or '', it is a string
+print("100")
+print(999 * 24, end="\n\n")     # Whole number
+
+
+# 3. Floating Point Integers
+print(8.2)
+print(.67)
+print(23.)
+print(4. * 12)  # float multiply by integers gives always float.
+print(99991, .465 * 21, end="\n\n")
+
+
+# 4. Boolean (True/False) # Camel Case
+
+print(True)
+print(False)
+print(55 < 923)
+print(55 > 923)
+print(5 < 9 > 8)
+print(9 * 9.9 == 9.9 * 9, end="\n\n")
+
+
+# Python Operators(7) Vs Comparison Operators (6)
+
+print(5 + 8) # Additions
+print(5 - 8, 8-5) # Subtractions
+print(8 / 8) # Division (Always in the float)
+print(8 * 8) # Multiplication
+print(8 % 5) # Modulus -- Reminder
+print(10 // 8) # Floor Division
+print(8 ** 3, end="\n\n")   # Power/Exponential
+
+# Comparison Operations (6)
+print(9 > 8)
+print(9 < 8)
+print(10 <= 25)
+print(10 >= 10)
+print(8 != 8) # not equal to
+print(8 == 8, end="\n\n")   # similarity
+
+
+# BOD-MAS Rules
+print(2+5-5*2/8-(85/3*5))
+print(9 % 6 % 2) # Left to Right rule
+print(2 * 3 % 5) # Left to Right rule
+print(2 ** 3 ** 2) # Right to left rule
+
+
+### Variables:
+# Variables are always start with strings
