f6b7d06a7c83812a4267f0f85486ef7b79258ce9
Shivadev96 <144408499+Shivadev96@users.noreply.github.com>
2023-10-04 17:20:04 +0530

    Update subash.py

--- subash.py
+++ subash.py
@@ -11,3 +11,78 @@ print(9.2)
 print(2<3)
 print(2<3)
 print("DEV")
+
+""""""""""""""""""""""""""""""""""""""""""
+
+# # source code
+# # print - function
+# # () - parentheses
+# print(5 + 5)
+#
+# #python modules
+# # 1. During installation
+# # 2. own repo
+# # 3. 3re party like GitHub.com
+#
+#
+# # Python data types or literals
+# # 1. string (characters)
+# print('Hello World')
+# print("Hello World")
+#
+# print("hello\nWorld")
+# print("Hello\tWorld")
+
+# # Positional arguments - separating arguments with comma(,)
+# print('Hello','World')
+#
+# # Escape Charactor (\)
+# print('I\'m', 'learning', 'python')
+#
+# # Keyword arguments
+# print('hello','World',end='')
+# print('hello','world',sep='')
+# print('hello','world', end='',sep='')
+#
+# ##
+#
+# # 2. Integer
+# print(50)
+# # integer/Fload/Boolean inside '' or "" , it is a string.
+# print("50")
+# print(1000 * 5)  # whole number
+#
+# # 3. Floating Point Integer
+# print(5.5)
+# print(.5)
+# print(9.)
+# print(9. * 5)
+#
+# # 4 . Boolean (True/False)
+# print(True)  # camel case
+# print(False)
+# print(5 > 9)
+# print(5 < 9 > 8)
+# print(5 > 9 > 8)
+# print(5 > 5)
+
+# Python operators (7)  vs   Comparison operators (6)
+print(8 + 8)  # Addition
+print(8 - 8)  # Subtraction
+print(8 * 8)  # Multiplication
+print(8 / 8)  # Division (The answer will always be is float)
+print(8 % 8)  # Modulus - Remainder
+print(8 ** 8)  # Power/Exponential
+print(8 // 8)  # Floor Division
+
+print(8 > 8)   # Greater than
+print(8 < 8)   # Less than
+print(8 <= 8)  # Less than or equal to
+print(8 >= 8)  # Greater than or equal to
+print(8 != 8)  # Not Equal to
+print(8 == 8)  # Similar
+
+
+# BODMAS RULE
+print(2+5-5*2/8-(85/3*5))
+print(85/3*5)
