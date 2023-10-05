4af7658d78c4f9e37beaf232f5f3f920e9953df8
sureshcrizayn <iloveu1000timez@gmail.com>
2023-10-05 14:44:09 -0700

    nothing to say

---
+++ suresh-03-10.py
@@ -0,0 +1,60 @@
+# Source Code
+# Print is a function
+print(5)
+
+# Python modules
+# 1. During installation
+# 2. Own repository
+# 3. 3rd Party like GitHub.com
+
+# Python Data Types or Python Literals
+# 1. String (when a word and number inside the  "" and '' it consider as a string)
+print('Hello World')
+print("Hello World")
+# print("3 5 5") <-- it also consider as string
+
+
+
+# Positional Arguments
+
+# "string" pakkathula comma ( , ) iruntha consider as a "positional argument"
+# print("ronaldo","is","GOAT",0,7)
+
+# Escape Character (\) "slash pakkathila word or number it won't escape"
+# (\n) it takes to next line
+# (\t) 3 space vidum (don't    expect   anything)
+
+print('I\'m', 'learning', 'Python')
+
+# Keyword Arguments
+# sep=""  (sep will work only in comma (,) place )
+# end=""  (
+print('Hello', 'World', end='\n')
+print('Hello', 'World', sep='\n')
+print('Hello', 'World', sep=' $$$ ', end='')
+print('Ending')
+
+# 2. Integer means number
+# int
+# int la words multiplication pannalam, but words multiplication thavira vera maths panna mudiyathu
+print(50+5)
+print("ronaldo" * 5)
+
+# 3. Floating Point Integer
+print(5.5)
+print(.5)
+print(9.)
+print(9. * 5)
+
+# 4. Boolean (True/False) # Camel Case
+print(True)
+print(False)
+
+# Python Operators (7) vs Comparison Operators (6)
+print(8 + 8)   # Addition
+print(8 - 8)   # Subtraction
+print(8 / 8)   # Division (The answer will always be in Float)
+print(8 * 8)   # Multiplication
+print(8 % 3)   # Modulus - Remainder
+print(8 ** 3)  # Power/Exponential
+print(10 // 8) # Floor Division
