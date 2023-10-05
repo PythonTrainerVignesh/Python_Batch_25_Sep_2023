98fd62923cef891657760b74a2ea06f729da5507
sushmareddy-1234 <145994626+sushmareddy-1234@users.noreply.github.com>
2023-10-05 22:05:05 +0530

    updated

--- sushma3_10_2023.py
+++ sushma3_10_2023.py
@@ -3,14 +3,18 @@
 print("hello", "world")
 print(50*5)
 
-#python data types
+#python data types or python literals
 #1.string(ch)
 print("HELLO WORLD")
+print('HELLO WORLD')
+print("HELLO '' WORLD")
+print('HELLO "" WORLD')
+print("Python's Class")
 print("python\nbatch")
 print("python\tbatch")
 
 #positional argument
-print('hello','world')
+print('hello','world',4, 7, 44.4, (6 + 7), true, false, (1 - 0)
 #escape character(\)
 print('I\'m', 'learning', 'python')
 print()
@@ -20,18 +24,41 @@ print('hello','world', end='')
 print('hello','world', sep='')
 print('hello','world', sep='', end='')
 
-#integer string
+# 2. integer string
 #integer,float,boolean inside ''or"",it is a string
 #integer means whole numbers
-#print(1,000*5)won't work
+#print(1,000*5)won't work, (1000*5)it work
+print("50")      
 
-#floating point integer
+# 3. floating point integer
 print(5.5)
 print(9.)
 print(5.*4)
+print(.5)
+print(99999, .95 * 5)    
 
-#boolean
+# 4. boolean or camel case
 print(True)
 print(False)
 print(9>5)
+print(9 * 9.9 == 9.9 * 9)      
 #variable never start with symbols and without gap
+
+# python operators (7) vs . comparsion operators (6)
+print(8 + 8) # addition
+print(8 - 8) # subtraction
+print(8 / 8) # division (the ansers will be always in float)
+print(8 * 8) # multiplication
+print(8 % 8) # modulus - remainder
+print(8 ** 8) # power/expotential
+print(10 // 8) # floor division
+
+#comparsion operators(6)
+print(8 > 8) #greater than
+print(8 < 8) #less than
+print(8 >= 8) #greater than or equal to
+print(8 <= 8) #less than or equal to
+print(8 != 8)  #not equal to
+ print(8 == 8) #similar     
+      
+      
