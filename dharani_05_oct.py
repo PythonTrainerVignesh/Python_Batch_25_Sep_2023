252fe9d6faadb33f9663966328bb2a7843ed9ec9
Dharani-12 <p.dharani225@gmail.com>
2023-10-05 17:53:32 +0530

    new ile

---
+++ dharani_05_oct.py
@@ -0,0 +1,24 @@
+John = 3
+Mary = 5
+Adam = 6
+total_apples = John + Mary + Adam
+
+print("Total number of apples:", total_apples)
+print(f"Total number of apples: {total_apples}")
+
+################## Round function
+Kilometers = 12.25
+Miles = 7.38
+
+miles_to_kilometer = Miles * 1.61
+kilometer_to_miles = Kilometers / 1.61
+
+print(f"{Miles} miles is {round(miles_to_kilometer,2)} kilometers")
+print(f"{Kilometers} kilometer is {round(kilometer_to_miles,2)} miles")
+
+
+x= 0
+
+y = (3 * x**3) - (2 * x**2) + (3 * x) - 1
+
+print(f"y = {y}")
