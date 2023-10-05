d6da1388697f30da366ca81a0d287f3f67110d4f
sushmareddy-1234 <145994626+sushmareddy-1234@users.noreply.github.com>
2023-10-05 12:37:53 +0530

    Add files via upload

---
+++ sushma3_10_2023.py
@@ -0,0 +1,37 @@
+#source code
+#print is a function
+print("hello", "world")
+print(50*5)
+
+#python data types
+#1.string(ch)
+print("HELLO WORLD")
+print("python\nbatch")
+print("python\tbatch")
+
+#positional argument
+print('hello','world')
+#escape character(\)
+print('I\'m', 'learning', 'python')
+print()
+
+#keyword arguments
+print('hello','world', end='')
+print('hello','world', sep='')
+print('hello','world', sep='', end='')
+
+#integer string
+#integer,float,boolean inside ''or"",it is a string
+#integer means whole numbers
+#print(1,000*5)won't work
+
+#floating point integer
+print(5.5)
+print(9.)
+print(5.*4)
+
+#boolean
+print(True)
+print(False)
+print(9>5)
+#variable never start with symbols and without gap
