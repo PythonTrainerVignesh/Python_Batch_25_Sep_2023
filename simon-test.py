00a72bd484b20f8fcc1abf2b026d100bca27d06b
unknown <ssimon68475@gmail.com>
2023-10-10 22:54:37 +0530

    finished

--- simon-test.py
+++ simon-test.py
@@ -1,30 +1,88 @@
-x= 10
-n=(1/(x+1/(x+1/(x+1/x))))
-print("y =",n)
+
+ # 1
+x = input("any gradution :")
+y = int(input("your age :"))
+if x == "yes" and 22 <= y <= 30 :
+     print(" you are eligible")
+else:
+     print("you are not eligible")
+ # 2
+laptop = input("you have laptop :")
+broadband = input("proper broadband :")
+if laptop == "yes" and broadband == "yes" :
+     print("atten online class")
+
+else:
+     print("atten offline class")
+ # 3
+water = input("if you have water :")
+if water == "yes":
+     print("drink it")
+
+else:
+     print("go to shop and get it")
+ # 4
+x = input("if it rain :")
+if x =="yes":
+     print("watch tv")
+else:
+     print("go out and play")
+
+ # 5
+raining = input("if rain :")
+both = input("lightly or heavily :")
+if raining == "yes" and both == "lightly" and "heavily" :
+     print("go for shopping")
+
+else:
+     print("otherwise sit at home")
+
+ # 6
+sunny = input("is it sunny :")
+temp = int(input("what temperature :"))
+if sunny== "yes" and temp <= 30 :
+    print("go to park")
+else:
+    print("otherwise stay at home")
+
+ # 7
+carrom = input("If You Play Carrom :")
+chess = input("If You Play Chess :")
+if carrom == "yes" or chess == "yes":
+     print("participate in the tournament")
+else:
+     print("otherwise go and study")
+
+# 8
+java = input("If You Know Java :")
+python = input("If You Know Python :")
+if java == "yes" or python == "yes":
+     print("Eligible To Apply For The Job")
+else:
+     print(" Not Eligible To Apply For The Job")
 
 
-
-x = 1.
-n = ((3*(x)**3)-(2*(x)**2)+(3*(x))-1)
-print("y =",n)
-
-n = 110
-x = (100 <= n >=200)
-print(x)
+ # 9
+certi = input("If The User Has A Certification :")
+if certi == "yes":
+     print("Invite the user")
+else:
+    print("Not invite the user")
 
 
+ # 10
+age = int(input("enter your age :"))
+license = input("do u have license :")
+LLR = input("do u have LLR :")
+if age >=17 and license=="yes" or LLR =="yes" :
+     print("he is eligible")
+else:
+    print("not elgible")
 
-hour = 23
-min = 58
-dur = 642
-end_hour = ((hour*60)+58+642)//60%24
-end_min = (min+dur)%60
-print("event end time is ",end_hour,":",end_min)
 
-
-hour = int(input("start hour :"))
-min = int(input("start min:"))
-dur = int(input("event duration:"))
-end_hour = ((hour*60)+min+dur)//60%24
-end_min = (min+dur)%60
-print("event end time is ",end_hour,":",end_min)
+# 11
+charger =int(input("Mobile charge is greater than 20% :"))
+if charger >= 20:
+     print("watch a movie")
+else:
+     print("charge the mobile")
