9796b2b36e336b2e09f3f7608494aab8fb8b52b5
PythonTrainerVignesh <vigneshkumar9797@gmail.com>
2023-10-11 17:31:29 +0530

    asdsad

--- School.py
+++ School.py
@@ -1,4 +1,21 @@
+age = input('Enter your age: ')
 
-
-teacher1 = Teachers()
-teacher1.
+if age.isdigit():
+    if int(age) >= 18:
+        License = input('Do you have a license (yes/no): ')
+        if License == 'yes':
+            print('You are eligible to drive.')
+        elif License == 'no':
+            LLR = input('Do you have LLR (yes/no): ')
+            if LLR == 'yes':
+                print('You are eligible to drive with a pillion rider.')
+            elif LLR == 'no':
+                print('You are not eligible to drive. Go & apply for a LLR first.')
+            else:
+                print('Invalid input.')
+        else:
+            print('Invalid input.')
+    else:
+        print('You are not eligible.')
+else:
+    print('Invalid input.')
