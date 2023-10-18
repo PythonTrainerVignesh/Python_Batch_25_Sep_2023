0738a4af2504a1e99f86f1be13e87dc8f065128c
sushma@-1234 <sushmareddymaruri@gmail.com>
2023-10-18 16:00:50 +0530

    new

--- sushma17_10_23.py
+++ sushma17_10_23.py
@@ -1,20 +1,30 @@
-# word = "im learning python"
+# word = "im learning python" #iterable
 # for no in range(0,100,2 * 1):
 #     print(f"2 x {no}={2*no}")
-#
-# word = "Mississippi"
-# count = 0
+# #
+# word = "Mississippi" #united states of america
+# count_s = 0
+# count_i = 0
 # for i in word:
 #     if i == 's':
-#         count = count + 1
-#         print(count)
-#     else i =='i'
-#         count = count + 1
-#         print(count)
+#         count_s = count_s + 1
+#     elif i == 'i':
+#         count_i = count_i + 1
+#     else:
+#         continue
+#     print(f"the no of s in {word} is {count_s}.")
+#     print(f"the no of i in {word} is {count_i}.",sep='\n')
 
-word = "united states of america"
-for i in word:
-     print(i *2, end='')
-word = "Hi-There"
-for i in word:
-    print()
+
+#
+tuple = (0,1,2,3,4,5,6) #immutable it will not change
+list = [0,1,2,3,4,5,6, [8,8,0,7,9],{"":""}] #mutable it will change
+dict = {"flowers":"jasmine",
+        "fruits":"apple",
+        "chocolates":"dairy milk"} #key-value pairs
+print(tuple[4])
+print(dict)
+print(list)
+print(list[4])
+list[4].replace = 9
+print(dict.keys())
