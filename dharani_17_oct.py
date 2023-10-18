1f2adeab7669ebe49cf901f43876cd9adfd208f2
Dharani-12 <p.dharani225@gmail.com>
2023-10-18 16:08:22 +0530

    changes

--- dharani_17_oct.py
+++ dharani_17_oct.py
@@ -16,12 +16,11 @@
 # for i in word:
 #     print(i)
 #
-# # next one: here range(0, 100 ,5):
-# #drr
-# # count = 1
-# # for i in range(2, 21, 2):
-# #     count = count +1
-# #     print(f"2 * {count} = {i}")
+# next one: here range(0, 100 ,5):
+# count = 1
+# for i in range(2, 21, 2):
+#     count = count +1
+#     print(f"2 * {count} = {i}")
 #
 # for i in range(1, 21):
 #     print(f"2 X {i} = {i * 2}")
@@ -64,13 +63,35 @@
 #     print(i + i, end='')
 #     # print(i * 2, end='')
 
+cs = 0
+fruits = ['apple', 'banana', 'cherry']
+for k in fruits:
+    cs = cs + 1
+    print(f"{cs}. {k}")
+
+num = [1, 2, 3, 4, 5, 6]
+total = 0
+for no in num:
+    total = total + no
+print("Sum:", total)
+
+names = ["Alice", "Bob", "Charles"]
+for j in names:
+    print(f"Hello, {j}!")
+
+Friuts = ['apple', 'banana', 'cherry']
+vowel_count = 0
+for word in Friuts:
+    for l in word:
+        if l in "aeiou":
+            vowel_count = vowel_count + 1
+print(f"Total vowels in {Friuts}: {vowel_count}")
+
 # next one:
 
-no_tuple = (1, 2, 3, 4, 5, 6)   # Immutable :- not changeable
-no_list = [1, 2, 3, 4, 5, 6]    # Mutable
+no_tuple = (1, 2, 3, 4, 5, 6)  # Immutable :- not changeable
+no_list = [1, 2, 3, 4, 5, 6]  # Mutable
 no_dict = {"fruit1": "Apple",
            "fruit2": "Mango",
            "fruit": "Banana",
            "email": "dharani@ita.in"}  # Key-Value pair
-
-
