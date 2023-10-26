3b75467e0bf1f9f9a6a0f2706996b17f65239ba7
sushmareddy-1234 <145994626+sushmareddy-1234@users.noreply.github.com>
2023-10-26 23:05:23 +0530

    Add files via upload

---
+++ sushma oops.py
@@ -0,0 +1,9 @@
+
+from sushma18_10_23 import (add,subtract,multiply)
+print(add(5,6))
+print(subtract(6,3))
+
+import sushma18_10_23
+print(sushma18_10_23.add(5,17))
+print(sushma18_10_23.subtract(7,3))
+
